1233219a8382f8b417ae84d27734bdf23057c9f5
313379 <itamarrochaa@yahoo.com>
2019-02-08 08:21:23 -0200

    Criando o primeiro JS

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,22 @@
 console.log(document.querySelector(".titulo"));
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Itamar Rocha";
+
+
+var paciente = document.querySelector("#primeiro-paciente");// dessa forma pegamos pelo id
+console.log(paciente);
+var tdPeso = paciente.querySelector(".info-peso");//dessa forma pegamos a classe
+var peso = tdPeso.textContent;
+
+console.log(tdPeso.textContent);
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura  = tdAltura.textContent;
+
+
+
+var imc = peso / (altura  * altura); 
+console.log('O valor do IMC é ' + imc );
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
